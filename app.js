0fcdbff5edbd5bcc7a3ee32084ee897981c2277e
Musfiq <weblead@coderstrustbd.bd>
2023-07-05 20:29:17 +0600

    version 1

---
+++ app.js
@@ -0,0 +1,20 @@
+// console.log(document.getElementById('one'))
+
+// console.log(document.getElementById('one').innerText)
+
+
+// console.log(document.getElementById('input-field'))
+
+
+
+
+
+function getValue(){
+
+    var value = document.getElementById('input-field').value;
+
+    var numberValue =  parseInt(value)
+
+
+     console.log(typeof(numberValue))
+}
